8018af35b25fd4ecbd3e8e2b42c78661f0f98ad6
xyz <you@example.com>
2020-05-04 22:16:28 +0800

    v1.0

--- processing/build/CMakeFiles/processing_test_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "processing_test_automoc.cpp"
-  "CMakeFiles/processing_test_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/processing_test_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
